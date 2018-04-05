323eb6feb65f58f40ce9b5e7c800dd3eda92914c
Mathias Voelcker <mathias_v94@hotmail.com>
2018-04-05 08:20:56 -0300

    recriar classe atributo

---
+++ src/app/atributo/atributo.ts
@@ -0,0 +1,9 @@
+export class Atributo {
+    public id: number;
+    public multiplicador: number;
+    public nome: string;
+    public obrigatorio: boolean;
+    public ordem: number;
+    public status: boolean;
+    public unidade: string;
+}
