b032cbc97fc4797b00e4ea34272cb1668f9f641b
Leonardo Marcelino Vieira <leonardo.marcelino@outlook.com>
2018-04-06 16:37:43 -0300

    WebResponsividade da Listagem de Grupos de Receita e Conexão com Backend

--- src/app/grupo/grupo-listagem/grupo-listagem.component.ts
+++ src/app/grupo/grupo-listagem/grupo-listagem.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GrupoReceita } from '../../ingrediente/grupo-receita';
+import { Grupo } from '../grupo';
+import { GrupoService } from '../grupo.service';
+import { Response } from '@angular/http/src/static_response';
 
 @Component({
   selector: 'app-grupo-listagem',
@@ -8,9 +11,17 @@ import { GrupoReceita } from '../../ingrediente/grupo-receita';
 })
 export class GrupoListagemComponent implements OnInit {
 
-  constructor() {
+  grupos: Grupo[];
+
+  constructor(private grupoService: GrupoService) {
    }
 
   ngOnInit() {
+    this.grupoService.buscarGrupos().subscribe(
+      response => {
+        this.grupos = response;
+        console.log (this.grupos);
+      }
+    )
   }
 }
--- src/app/ingrediente/ingrediente-listagem/ingrediente-listagem.component.ts
+++ src/app/ingrediente/ingrediente-listagem/ingrediente-listagem.component.ts
@@ -18,7 +18,7 @@ export class IngredienteListagemComponent implements OnInit {
   ngOnInit() {
     this.ingredienteService.buscarIngredientes().subscribe(
       response => { 
-      //  this.ingredientes = response.Ingredientes;
+        this.ingredientes = response;
         console.log(this.ingredientes);
       });
   }
