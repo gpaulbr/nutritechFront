11928de39f6a70747e5b5fb42df6494ecac82bc8
Leonardo Marcelino Vieira <leonardo.marcelino@outlook.com>
2018-04-18 19:14:34 -0300

    ftp.service implementado

--- src/app/ftp/ftp.service.ts
+++ src/app/ftp/ftp.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import { NUTRITECH_API } from '../app.api';
+import { Ftp } from './ftp';
 
 @Injectable()
 export class FtpService {
 
   private url = NUTRITECH_API + "/receitas";
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  salvarFTP(ftp: Ftp): Observable<Ftp>{
+    return this.http.post<Ftp>(this.url, ftp);
+  }
+
+  buscarFTP(): Observable<Ftp[]>{
+    return this.http.get<Ftp[]>(this.url);
+  }
 
 }
