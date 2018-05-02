beb34b67764cf69a6bfcec2a5a5faa8482b06285
bianca <bianca.camargo@acad.pucrs>
2018-05-02 18:10:11 -0300

    buscando por nome do ingrediente

--- src/app/ingrediente/ingrediente-listagem/ingrediente-listagem.component.ts
+++ src/app/ingrediente/ingrediente-listagem/ingrediente-listagem.component.ts
@@ -1,39 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ingrediente } from '../ingrediente';
 import { IngredienteService } from '../ingrediente.service';
 import { Router } from '@angular/router';
+import { Usuario } from '../../usuario/usuario';
+import { TipoUsuario } from '../../usuario/tipo-usuario.enum';
+import { id } from '@swimlane/ngx-datatable/release/utils';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 @Component({
-  selector: 'app-ingrediente-listagem',
+  selector: 'filter-demo',
   templateUrl: './ingrediente-listagem.component.html',
   styleUrls: ['./ingrediente-listagem.component.css']
 })
 export class IngredienteListagemComponent implements OnInit {
+  ingredientes: Ingrediente[];
+  usuarioLogado: Usuario;
+  
   rows = [];
   columns = [
     { name: 'Nome' },
     { name: 'Origem' },
     { prop: 'criador.nome', name: "Criador" },
+    { prop: 'criador.id', name: "idCriador" },
     { name: 'Tipo' }
   ];
 
-  ingredientes: Ingrediente[];
+  @ViewChild(DatatableComponent) table: DatatableComponent;
 
   constructor(
     private router: Router,
     private ingredienteService: IngredienteService) { }
 
-  ngOnInit() {
-    var usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
-    if(usuarioLogado == null) {
+   ngOnInit() {
+  
+    this.usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
+    if(this.usuarioLogado == null) {
       this.router.navigate(['./']);
     }
+
     this.ingredienteService.buscarIngredientes().subscribe(
       response => { 
-       this.ingredientes = response['Ingredientes'];
-       this.rows = response['Ingredientes'];
-        console.log(this.ingredientes);
+        this.ingredientes = response['Ingredientes'];
+
+        let lista = []
+
+        response['Ingredientes'].forEach(p => {
+          if(this.usuarioLogado.tipo !== "ADMIN") {
+            if(p.tipo === "PRIVADO") {
+              if(p.criador.id === this.usuarioLogado.id) {
+                lista.push(p)
+              }
+            } else {
+              lista.push(p)
+            }
+          } else {
+            lista.push(p)
+          }
+          
+        })
+
+        this.rows = lista
       });
+    
+  console.log("Usuário logado:" + this.usuarioLogado.tipo);//OK TIRAR
+  }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.ingredientes.filter(function(d) {
+      return d.nome.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
   }
 
 }
--- src/app/usuario/tipo-usuario.enum.ts
+++ src/app/usuario/tipo-usuario.enum.ts
@@ -2,4 +2,8 @@ export enum TipoUsuario {
     Admin = 0,
     Usuario = 1,
     Professor = 2, 
+
+    ADMIN = 'ADMIN',
+    USER = 'USER',
+    PROFESSOR = 'PROFESSOR'
 }
