7ff8c5f40404fc1a971bc36b8ee2bdcb37755659
Leonardo Marcelino Vieira <leonardo.marcelino@acad.pucrs.br>
2018-05-03 15:24:43 -0300

    Gerado componente de seleção de ingredientes

---
+++ src/app/ftp/ftp-selecao-ingredientes/ftp-selecao-ingredientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ftp-selecao-ingredientes',
+  templateUrl: './ftp-selecao-ingredientes.component.html',
+  styleUrls: ['./ftp-selecao-ingredientes.component.css']
+})
+export class FtpSelecaoIngredientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
