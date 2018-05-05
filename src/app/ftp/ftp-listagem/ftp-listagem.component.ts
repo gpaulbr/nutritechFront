2fbd2107be6ca14839854b718744e21aeaaba5a3
bianca <bianca.camargo@acad.pucrs>
2018-05-04 23:19:24 -0300

    busca por qualquer campo finalizada e troca true/false por ativa/inativa no status (somente visual)

--- src/app/ftp/ftp-listagem/ftp-listagem.component.ts
+++ src/app/ftp/ftp-listagem/ftp-listagem.component.ts
@@ -6,7 +6,6 @@ import { Usuario } from '../../usuario/usuario';
 import { TipoUsuario } from '../../usuario/tipo-usuario.enum';
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
-import { create } from 'domain';
 
 
 @Component({
@@ -25,7 +24,6 @@ export class FtpListagemComponent implements OnInit {
     { name: 'Status' },
     { name: 'Tipo' },
     { prop: 'criador', name: "Criadores"},
-    //{ prop: "'MERADA'", name: "Criadores" },
     { prop: 'grupoReceita.nome', name: "Grupo" }
   ];
 
@@ -50,30 +48,34 @@ export class FtpListagemComponent implements OnInit {
           p['criadores'].forEach(u => {
             stringCriadores += u.nome + "<br>"; // concatena cada nome de criador de receita
           });
-          p['criador'] = stringCriadores
-          //p['criador'] = p['criadores'][0].nome;
+          p['criador'] = stringCriadores;
+          if(p["status"]){//para ficar mais familar para o usuário troco 'true' por 'Ativa', para a exibição
+            p["status"] = 'Ativa';
+          }else if(!p["status"])//para ficar mais familar para o usuário troco 'false' por 'Inativa' para a exibição
+          {
+            p["status"] = 'Inativa';
+          }
           lista.push(p);
-          //console.log( p['criador']);
         })
-        //console.log(lista);
         this.rows = lista;
     });
-    //console.log(criador.id);
     }
 
         updateFilter(event) {
           const val = event.target.value.toLowerCase();
-      
+          let i=0;
           // filtra por todos os campos da tabela
           const temp = this.receitas.filter(function(d) {
             if(d.nome.toLowerCase().indexOf(val) !== -1 || !val)
                   return d.nome.toLowerCase().indexOf(val) !== -1 || !val;
             if(d.rendimento.toString().toLowerCase().indexOf(val) !== -1 || !val)
                   return d.rendimento.toString().toLowerCase().indexOf(val) !== -1 || !val;
-            /*else if(d.criador.toLowerCase().indexOf(val) !== -1 || !val) 
-                  return d.criador.toLowerCase().indexOf(val) !== -1 || !val;*/
+            else if(d.criadores[0].nome.toLowerCase().indexOf(val) !== -1 || !val)
+                return d.criadores[0].nome.toLowerCase().indexOf(val) !== -1 || !val;
             else if(d.tipo.toString().toLowerCase().indexOf(val) !== -1 || !val)
                   return d.tipo.toString().toLowerCase().indexOf(val) !== -1 || !val;
+            if(d.status.toString().toLowerCase().indexOf(val) !== -1 || !val)
+                  return d.status.toString().toLowerCase().indexOf(val) !== -1 || !val;
           
           });
       
